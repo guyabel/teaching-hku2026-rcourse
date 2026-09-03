954b65aeece3d96104fb2f0f22bc6c3cdd23888b
Guy Abel <g.j.abel@gmail.com>
2026-09-03 08:32:00 +0800

    class1 solutions

---
+++ exercise-solution/ex022_functions.R
@@ -0,0 +1,16 @@
+# 1. Natural logarithm of 5
+log(x = 5)
+# 2. Square root of 121
+sqrt(x = 121)
+# 3. Absolute value of 10 and -11
+abs(x = 10); abs(x = -11)
+# 4. 8 x 7 x 6 x 5 x 4 x 3 x 2 x 1 (the factorial of 8)
+factorial(x = 8)
+# 5. Round pi (3.141593) to 3 decimal places
+round(x = pi, digits = 3); round(x = 3.141593, digits = 3)
+# 6. The logarithm of 100 to the base 10
+log10(x = 100)
+# 7. The exponential of 4
+exp(x = 4)
+# 8. Use the print() function to display your name
+print("Guy Abel")
---
+++ exercise-solution/ex023_logic.R
@@ -0,0 +1,16 @@
+# 1. Test if 3 is larger than pi
+3 > pi
+# 2. Test if pi is equal to 3.141593
+pi == 3.141593
+# 3. Test if 7 divided by 3 is less than or equal to 3
+7/3 <= 3
+# 4. Test if 5 times 2 is greater than or equal to 10
+5*2 >= 10
+# 5. Test if 1 plus 5 is not equal to 7
+1 + 5 != 7
+# 6. Test if logarithm of 1000 is larger 7
+log(x = 1000) > 7
+# 7. Test if pi is greater than 3 and 4
+pi > 3 &  pi > 4
+# 8. Test if pi is less than 3 or 4
+pi < 3 |  pi < 4
